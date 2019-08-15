c5fa84eaa31b24d89ccb7dc971803796dbd46d63
xuebaopeng <xuebaopeng@jd.com>
2019-08-15 15:21:30 +0800

    redis测试

---
+++ src/main/java/redis/RedisJava.java
@@ -0,0 +1,14 @@
+package redis;
+
+import redis.clients.jedis.Jedis;
+
+public class RedisJava {
+    public static void main(String[] args) {
+        //连接本地的 Redis 服务
+        Jedis jedis = new Jedis("94.191.124.241");
+        System.out.println("连接成功");
+        //查看服务是否运行
+        System.out.println("服务正在运行: " + jedis.ping());
+
+    }
+}
