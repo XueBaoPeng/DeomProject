0f073b22a688e5bd3b920c2ef85f3ed5257b5bba
xuebaopeng <xuebaopeng@jd.com>
2019-11-05 14:47:36 +0800

    jvm内存 测试

---
+++ src/main/java/JavaVmStackSOF.java
@@ -0,0 +1,26 @@
+/**
+ * @Auther: xuebaopeng
+ * @Date: 2019/10/25 17:43
+ * @Description:虚拟机栈和本地方法栈OOM测试
+ * VM Args: -Xss128k
+ */
+public class JavaVmStackSOF {
+
+    private int stackLength=1;
+
+    public void stackLeak(){
+        stackLength++;
+        stackLeak();
+    }
+
+    public static void  main(String args[] ) throws Throwable{
+        JavaVmStackSOF javaVmStackSOF=new JavaVmStackSOF();
+        try {
+            javaVmStackSOF.stackLeak();
+        }catch (Throwable e){
+            System.out.println("stack length"+javaVmStackSOF.stackLength);
+            throw e;
+        }
+    }
+
+}
