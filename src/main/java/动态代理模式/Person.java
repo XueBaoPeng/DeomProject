db97e66fa34d49e2b1e8d7eab2ff95f61dc30f4e
xuebaopeng <xuebaopeng@jd.com>
2019-11-26 11:00:40 +0800

    java 动态代理模式

---
+++ src/main/java/动态代理模式/Person.java
@@ -0,0 +1,10 @@
+package 动态代理模式;
+
+/**
+ *  * 创建Person接口
+ */
+public interface Person {
+    void giveMoney();
+
+    void sayHello();
+}
---
+++ src/main/java/静态代理模式/Person.java
@@ -0,0 +1,9 @@
+package 静态代理模式;
+
+/**
+ *  * 创建Person接口
+ */
+public interface Person {
+    void giveMoney();
+
+}
