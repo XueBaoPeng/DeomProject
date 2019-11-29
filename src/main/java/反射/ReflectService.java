8de02dd499ef7f48269d3c28bc67dcb6638e90b9
xuebaopeng <xuebaopeng@jd.com>
2019-11-29 10:30:22 +0800

    java 反射

---
+++ src/main/java/反射/ReflectService.java
@@ -0,0 +1,22 @@
+package 反射;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class ReflectService {
+
+
+    public void sayHello(String name){
+        System.out.println("hello"+name);
+    }
+
+    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        //通过反射创建ReflectService对象.
+        Object service=Class.forName("反射.ReflectService").getInterfaces();
+
+        Method method=service.getClass().getMethod("sayHello",String.class);
+
+        method.invoke(service,"sss");
+    }
+
+}
