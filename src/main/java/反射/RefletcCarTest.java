d7b1f89ee9c508f2be5ee12d97cea24ae5c7aa94
xuebaopeng <xuebaopeng@jd.com>
2020-01-15 10:28:47 +0800

    反射

--- src/main/java/反射/RefletcCarTest.java
+++ src/main/java/反射/RefletcCarTest.java
@@ -24,6 +24,10 @@ public class RefletcCarTest {
     }
 
     public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        ClassLoader loader = Thread.currentThread().getContextClassLoader();
+        System.out.println("current loader:"+loader);
+        System.out.println("parent loader:"+loader.getParent());
+        System.out.println("grandparent loader:"+loader.getParent(). getParent());
         Car car = initByDefaultConst();
         car.introduce();
     }
